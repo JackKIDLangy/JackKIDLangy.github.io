78dd10d8e1b747e4340c08e1add7ac485f497254
Kenta Yokota <langy.ky@gmail.com>
2023-01-20 00:52:54 +0900

    add and links

--- JackKIDLangy.js
+++
@@ -1,7 +0,0 @@
-const toggleMenu = () => {
-    const menu = document.querySelector('.menu');
-    menu.classList.toggle('open');
-}
-
-const menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener('click', toggleMenu); 
